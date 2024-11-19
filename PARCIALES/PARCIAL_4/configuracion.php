3e3e45e3fd92412124fd99cec9d4d81fb4e9aded
nicolas-villarreal-utp <nicolas.villarreal@utp.ac.pa>
2024-11-18 20:22:04 -0500

    20241118 2021 - Parcial 4

--- PARCIALES/PARCIAL_4/configuracion.php
+++ PARCIALES/PARCIAL_4/configuracion.php
@@ -4,10 +4,11 @@ require_once 'vendor/autoload.php';
 
 //leer secrets en json file
 $secrets = json_decode(file_get_contents('secrets.json'), true);
+$google_client_id = $secrets['google_client_id'];
 $google_client_secret = $secrets['google_client_secret'];
 $google_books_secret = $secrets['google_books_secret'];
 
-$clientID = '486600931595-dfq94aso1nmuk3imhbv91ott7fqemh5e.apps.googleusercontent.com';
+$clientID = $google_client_id;
 $clientSecret = $google_client_secret;
 $redirectUri = 'http://localhost/PARCIALES/PARCIAL_4/index.php';
 $apiKey = $google_books_secret;
